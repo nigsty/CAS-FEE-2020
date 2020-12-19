6955ddee7fc3d8f76ff3383617cd394981c13446
Nigsty Abreha <nigsty_e@yahoo.com>
2020-12-19 13:13:13 +0100

    20200603HttpNode

--- 20200527ES6ModularisierungImBrowser/Demos/Demo1_OO/public/scripts/bl/calculation-result.js
+++
@@ -1,8 +0,0 @@
-class CalculationResult {
-    constructor(left = 0, right = 0, result = 0, error = null) {
-        this.left = left;
-        this.right = right;
-        this.result = result;
-        this.error = error;
-    }
-}
--- 20200527ES6ModularisierungImBrowser/Demos/Demo1_OO/public/scripts/bootstrapper.js
+++
@@ -1,9 +0,0 @@
-class Bootstrapper {
-    static start() {
-        new CalculatorController().calcAction();
-    }
-}
-
-// wait until scripts have been loaded
-document.addEventListener('DOMContentLoaded', Bootstrapper.start);
-
